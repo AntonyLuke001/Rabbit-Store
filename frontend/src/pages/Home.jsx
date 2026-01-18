6ddc3e4ec19846cdc8bd2c1f97a79e3f7a09ac4e
Antony Luke <antonyluke001@gmail.com>
2026-01-18 23:42:49 +0530

    home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Hero from '../components/layout/Hero'
 import GenderCollectionSection from '../components/products/GenderCollectionSection'
 import NewProducts from '../components/products/NewProducts'
@@ -6,74 +6,41 @@ import ProductDetails from '../components/products/ProductDetails'
 import ProductGrid from '../components/products/ProductGrid'
 import FeaturedCollection from '../components/products/FeaturedCollection'
 import FeaturesSection from '../components/products/FeaturesSection'
-const placeholderproducts = [
-  {
-    _id:1,
-    name:"Product 1",
-    price:100,
-    images:[{
-      url:"https://picsum.photos/500/500?random=1"
-    }
-  ]},
-    {
-    _id:2,
-    name:"Product 2",
-    price:100,
-    images:[{
-      url:"https://picsum.photos/500/500?random=2"
-    }
-    ]},
-    {
-    _id:3,
-    name:"Product 3",
-    price:100,
-    images:[{
-      url:"https://picsum.photos/500/500?random=3"
-    }
-    ]},
-     {
-    _id:4,
-    name:"Product 4",
-    price:100,
-    images:[{
-      url:"https://picsum.photos/500/500?random=4"
-    }
-    ]},
-     {
-    _id:5,
-    name:"Product 5",
-    price:100,
-    images:[{
-      url:"https://picsum.photos/500/500?random=5"
-    }
-    ]},
-     {
-    _id:6,
-    name:"Product 6",
-    price:100,
-    images:[{
-      url:"https://picsum.photos/500/500?random=5"
-    }
-    ]},
-     {
-    _id:7,
-    name:"Product 7",
-    price:100,
-    images:[{
-      url:"https://picsum.photos/500/500?random=7"
-    }
-    ]},
-     {
-    _id:8,
-    name:"Product 8",
-    price:100,
-    images:[{
-      url:"https://picsum.photos/500/500?random=8"
-    }
-    ]},
-  ];
+import { useDispatch,useSelector } from 'react-redux'
+import { fetchProductsByFilters } from '../redux/slices/productsSlice'
+import axios from "axios"
 
 const Home = () => {
+
+  const dispatch = useDispatch();
+  const { products,loading,error } = useSelector((state) => state.products);
+  const [bestSellerProduct, setBestSellerProduct] = useState(null);
+
+  useEffect(()=>{
+    dispatch(
+      fetchProductsByFilters({
+        gender : "Women",
+        category : "Topwear",
+        limit:8
+      })
+    )
+    const fetchBestSeller = async()=>
+    {
+      try{
+        const response = await axios.get(
+          `${import.meta.env.VITE_BACKEND_URL}/api/products/best-seller`
+        );
+        
+        setBestSellerProduct(response.data);
+      }
+      catch(error)
+      {
+        console.error("Error fetching best seller:",error);
+      }
+    }
+    fetchBestSeller();
+  },[dispatch])
+
   return (
     <div>
         <Hero />
@@ -81,12 +48,16 @@ const Home = () => {
         <NewProducts/>
 
         <h2 className='text-3xl font-bold text-center mb-4' >Best Seller</h2>
-        <ProductDetails/>
+        {
+          bestSellerProduct?(
+            <ProductDetails product={bestSellerProduct} />) : 
+            (<p className='text-center' >Loading best seller...</p>)
+        }
 
         <div className='container mx-auto' >
           <h2 className='text-3xl text-center font-bold mb-4' >Top Wears for Women</h2>
         
-        <ProductGrid products={placeholderproducts} />
+        <ProductGrid products={products} loading={loading} error={error} />
         </div>
         <FeaturedCollection/>
         <FeaturesSection />
